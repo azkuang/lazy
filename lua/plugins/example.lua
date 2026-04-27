ff5ae841892751988783a306f0ca9a88e0b29a82
azkuang <alexzkuang0314@mgail.com>
2026-04-27 11:23:31 -0400

    lazy config updates

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -10,7 +10,11 @@ if true then return {} end
 -- * override the configuration of LazyVim plugins
 return {
   -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = true,
+  },
 
   -- Configure LazyVim to load gruvbox
   {
